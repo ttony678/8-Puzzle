8c9e3ccfa9612bc809e3837a5bf273fb0f65eaef
ttony678 <ttony678@yahoo.com>
2017-10-24 00:25:40 -0700

    Changed from int** to int[][]

--- 8puzzlesolver.cpp
+++ 8puzzlesolver.cpp
@@ -8,7 +8,9 @@
  * DUE: November 3rd, 2017
  */
 
+#include <unordered_map>
 #include <iostream>
+#include <functional>
 #include <string>
 #include <sstream>
 #include <vector>
@@ -39,7 +41,7 @@ const int doable[3][3] = { { 0, 1, 2 },
 struct Node {
     int g;                  // Holds g(n) for uniform function
     int h;                  // Holds h(n) for heuristic function
-    int** puzzle;           // Points to puzzle 
+    int puzzle[SIZE][SIZE];           // Points to puzzle 
     Node* parent;           // Points to parent for backtrace
 
     Node() {
@@ -47,21 +49,17 @@ struct Node {
         h = 0;
         parent = NULL;
 
-        puzzle = new int*[SIZE];
         for (int i = 0; i < SIZE; i++) {
-            puzzle[i] = new int[SIZE];
+            for (int j = 0; j < SIZE; j++) {
+                puzzle[i][j] = 0;
+            }
         }
     }
 
     // Used to create the first Node.
-    Node(int g, int h, int** newPuzzle, Node* p) 
+    Node(int g, int h, int newPuzzle[][SIZE], Node* p) 
             : g(g), h(h), parent(p) {
-        
-        puzzle = new int*[SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            puzzle[i] = new int[SIZE];
-        }
-
+ 
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 puzzle[i][j] = newPuzzle[i][j];
@@ -71,12 +69,7 @@ struct Node {
 
     // Used to create more Nodes easily
     Node(Node* newNode) : g(newNode->g), h(newNode->h), parent(newNode->parent){
-        
-        puzzle = new int*[SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            puzzle[i] = new int[SIZE];
-        }
-
+  
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 puzzle[i][j] = newNode->puzzle[i][j];
@@ -95,45 +88,63 @@ struct Node {
         return true;
     }
 
+    bool operator==(const Node& i) const {
+        return ( puzzle[0][0] == i.puzzle[0][0] &&
+                 puzzle[0][1] == i.puzzle[0][1] &&
+                 puzzle[0][2] == i.puzzle[0][2] &&
+                 puzzle[1][0] == i.puzzle[1][0] &&
+                 puzzle[1][1] == i.puzzle[1][1] &&
+                 puzzle[1][2] == i.puzzle[1][2] &&
+                 puzzle[2][0] == i.puzzle[2][0] &&
+                 puzzle[2][1] == i.puzzle[2][1] &&
+                 puzzle[2][2] == i.puzzle[2][2]    );
+    }
+
     ~Node() {
-        for(int i = 0; i < SIZE; ++i) {
-            delete [] puzzle[i];
-        }
-        delete [] puzzle;
         delete parent;
     }
 };
 
-struct CompareAge {
+// Comparison function to set priority queue ordering.
+struct Least {
     bool operator()(Node *lhs, Node *rhs) {
         return (lhs->g + lhs->h) > (rhs->g + rhs->h);
     }
 };
 
+struct KeyHasher {
+    std::size_t operator() (const Node& n) const  {
+        string temp;
+        temp += to_string(n.g);
+        temp += to_string(n.h);
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                string s = to_string(n.puzzle[i][j]);
+                temp += s;
+            }
+        }
+        return hash<string>{}(temp);
+    }
+};
 
 // Taken from 
 // https://stackoverflow.com/questions/6339970/c-using-function-as-parameter
-typedef int (* vFunctionCall)(int** args); 
+typedef int (* vFunctionCall)(int args[][SIZE]); 
 
 char DisplayPuzzleTypeOptions();
-void PrintPuzzle(int**);
-void PrintNode(const Node*);
+void PrintPuzzle(int[][SIZE]);
+void PrintNode(Node*);
 void Expand(Node*, vFunctionCall f);
-int MissPlacedTile(int**);
-int ManhattanDistance(int**);
-int NoHeuristic(int**);
-Node* GeneralSearch(int**, vFunctionCall f);
+int MissPlacedTile(int[][SIZE]);
+int ManhattanDistance(int[][SIZE]);
+int NoHeuristic(int[][SIZE]);
+Node* GeneralSearch(int[][SIZE], vFunctionCall f);
 
-// Holds the min of f(n) = g(n) + h(n)
-priority_queue<Node*, vector<Node*>, CompareAge> tree;  
-
+priority_queue<Node*, vector<Node*>, Least> pq;  
+int nodes_dequeued = 0;
 
 int main() {
-    int** p;
-    p = new int*[SIZE];
-    for (int i = 0; i < SIZE; i++) {
-        p[i] = new int[SIZE];
-    }
+    int p[SIZE][SIZE];
 
     p[0][0] = 1;
     p[0][1] = 2;
@@ -141,26 +152,58 @@ int main() {
     p[1][0] = 4;
     p[1][1] = 5;
     p[1][2] = 6;
-    p[2][0] = 8;
-    p[2][1] = 7;
+    p[2][0] = 7;
+    p[2][1] = 8;
     p[2][2] = 0;
+
+    int t[SIZE][SIZE];
+
+    t[0][0] = 1;
+    t[0][1] = 2;
+    t[0][2] = 3;
+    t[1][0] = 4;
+    t[1][1] = 5;
+    t[1][2] = 6;
+    t[2][0] = 7;
+    t[2][1] = 8;
+    t[2][2] = 0;
+
+
+    Node* a = new Node(0, 0, p, NULL);
+    Node* b = new Node(0, 0, t, NULL); 
+
+    cout << "Hash of a: " << KeyHasher{}(a) << endl;
+    cout << "Hash of b: " << KeyHasher{}(b) << endl;
+
+    unordered_map<Node, bool, KeyHasher> mp;
+    mp[*a] = true;
+
+    cout << "Hash table lookup of a: " << mp[*a] << endl;
+    cout << "Hash table lookup of b: " << mp[*b] << endl;
+
+
 
     Node* n = GeneralSearch(p, (vFunctionCall)MissPlacedTile); 
 
-    if (n == NULL) {
+    if (n != NULL) {
+        cout << "Goal!!" << endl << endl;
+        cout << "To solve this problem, the search expanded a total of " <<
+                (nodes_dequeued - 1) << " nodes." << endl;
+        cout << "The depth of the goal was " << n->g << "." << endl;
+    }
+    else {
         cout << "No solution" << endl;
     }
+
     // char puzzleType = DisplayPuzzleTypeOptions();
 
-    for(int i = 0; i < SIZE; ++i) {
-            delete [] p[i];
-        }
-    delete [] p;
+   
     delete n;
     return 0;
 }
 
-Node* GeneralSearch(int** p, vFunctionCall f) {
+
+Node* GeneralSearch(int p[][SIZE], vFunctionCall f) {
 
     // Making a initial node with: 
     // - Uniform cost set to 0.
@@ -170,25 +213,17 @@ Node* GeneralSearch(int** p, vFunctionCall f) {
     // - A copy of the initial puzzle from main.
     // - It's parent attribute set to NULL to signify root of tree.
     Node* initial = new Node(0, f(p), p, NULL);
-    tree.push(initial);
-    tree.push(initial);
+    pq.push(initial);
+    pq.push(initial);
     
-    /*
-    tree.pop();
-    cout << "Tree size: " << tree.size();
-    cout << "\t Tree empty(): " << tree.empty() << endl;
-    cout << "Accessing empty tree: " << tree.top()->puzzle[0][0] << endl;;
-    */
-
-    while (!tree.empty()) {
-        if (tree.empty()) { return NULL; }
-        tree.pop();
-        // PrintNode(tree.top());
-        if (tree.top()->CheckGoal()) { return tree.top(); }
-        Expand(tree.top(), f);
+    while (!pq.empty()) {
+        if (pq.empty()) { return NULL; }
+        pq.pop();
+        ++nodes_dequeued;
+        PrintNode(pq.top());
+        if (pq.top()->CheckGoal()) { return pq.top(); }
+        Expand(pq.top(), f);
     }
-
-    return initial;
 }
 
 // This function checks if the current Node is at the goal state. If not,
@@ -217,7 +252,7 @@ void Expand(Node* n, vFunctionCall f) {
         newNode->puzzle[xpos-1][ypos] = 0;
         newNode->g++;
         newNode->h = f(newNode->puzzle);
-        tree.push(newNode);
+        pq.push(newNode);
     }      
 
     // Moving Tile DOWN
@@ -228,7 +263,7 @@ void Expand(Node* n, vFunctionCall f) {
         newNode->puzzle[xpos+1][ypos] = 0;
         newNode->g++;
         newNode->h = f(newNode->puzzle);
-        tree.push(newNode);
+        pq.push(newNode);
     }
     
     
@@ -240,7 +275,7 @@ void Expand(Node* n, vFunctionCall f) {
         newNode->puzzle[xpos][ypos-1] = 0;
         newNode->g++;
         newNode->h = f(newNode->puzzle);
-        tree.push(newNode);
+        pq.push(newNode);
     }
 
     
@@ -252,7 +287,7 @@ void Expand(Node* n, vFunctionCall f) {
         newNode->puzzle[xpos][ypos+1] = 0;
         newNode->g++;
         newNode->h = f(newNode->puzzle);
-        tree.push(newNode);
+        pq.push(newNode);
     }
 }
 
@@ -274,7 +309,7 @@ char DisplayPuzzleTypeOptions() {
     return puzzleType;
 }
 
-void PrintPuzzle(int** p) {
+void PrintPuzzle(int p[][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             if (p[i][j] != 0) {
@@ -289,13 +324,13 @@ void PrintPuzzle(int** p) {
     cout << endl;
 }
 
-void PrintNode(const Node* n) {
+void PrintNode(Node* n) {
     cout << "\tUniform cost: " << n->g << endl;
     cout << "\tHeuristic Value: " << n->h << endl;
     PrintPuzzle(n->puzzle);
 }
 
-int MissPlacedTile(int** p) {
+int MissPlacedTile(int p[][SIZE]) {
     int temp = 0;
 
     for (int i = 0; i < SIZE; i++) {
@@ -308,7 +343,7 @@ int MissPlacedTile(int** p) {
     return temp;
 }
 
-int ManhattanDistance(int** p) {
+int ManhattanDistance(int p[][SIZE]) {
     int temp = 0;
     int xpos = 0;
     int ypos = 0;
@@ -331,7 +366,7 @@ int ManhattanDistance(int** p) {
     return temp;
 }
 
-int NoHeuristic(int** p) {
+int NoHeuristic(int p[][SIZE]) {
     return 0;
 }
 
